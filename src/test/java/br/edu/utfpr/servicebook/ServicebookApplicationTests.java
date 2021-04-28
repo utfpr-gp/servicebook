e462071b21f9a05a79fe628880ab9f179678c6c2
CristhianAlbary <cristhian.albary@gmail.com>
2021-04-28 19:08:27 -0300

    Initial commit

---
+++ src/test/java/br/edu/utfpr/servicebook/ServicebookApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.utfpr.servicebook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServicebookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
