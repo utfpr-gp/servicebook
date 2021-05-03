605874e1df9f45daada3f68f71d743f6fdd7d9af
Guilherme Henrique <guilhermen@alunos.utfpr.edu.br>
2021-05-03 20:07:50 -0300

    configurado a tela inicial

---
+++ src/main/java/br/edu/utfpr/servicebook/controller/IndexController.java
@@ -0,0 +1,29 @@
+package br.edu.utfpr.servicebook.controller;
+import br.edu.utfpr.servicebook.model.entity.City;
+import br.edu.utfpr.servicebook.service.CityService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+@RequestMapping("/")
+@Controller
+public class IndexController {
+
+    @Autowired
+    private CityService cityService;
+
+
+    @GetMapping
+    public ModelAndView showForm(){
+        ModelAndView mv = new ModelAndView("visitor/index");
+        List<City> cities = cityService.findAll();
+        mv.addObject("cities", cities);
+
+        return mv;
+    }
+}
+
--- src/main/webapp/assets/resources/scripts/main.js
+++ src/main/webapp/assets/resources/scripts/main.js
@@ -1,16 +1,8 @@
 $(document).ready(function (){
 
     $('#select-city').change(function(i){
-        var selectedCountry = $(this).children("option:selected").val();
-        if (selectedCountry == 1) {
-            $('#img-city').attr("src", "assets/resources/images/guarapuava.jpg")
-        }
-        if (selectedCountry == 2) {
-            $('#img-city').attr("src", "assets/resources/images/curitiba.jpg")
-        }
-        if (selectedCountry == 3) {
-            $('#img-city').attr("src", "assets/resources/images/saoPaulo.jpg")
-        }
+        let selectedCountry = $(this).children("option:selected").val();
+        $('#img-city').attr("src", selectedCountry);
     });
 
     $(document).on('input change', '#input-range', function() {
