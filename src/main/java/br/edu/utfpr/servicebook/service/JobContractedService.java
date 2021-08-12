5e34853e07891c05fcb87018e085b89606f6bc7b
André <andredesouzaaranda@gmail.com>
2021-08-12 20:47:02 -0300

    Adicionado services para JobContracted
    Contabilização de serviços, avaliações e comentários

--- src/main/java/br/edu/utfpr/servicebook/service/JobContractedService.java
+++ src/main/java/br/edu/utfpr/servicebook/service/JobContractedService.java
@@ -13,23 +13,32 @@ public class JobContractedService {
     @Autowired
     private JobContractedRepository jobContractedRepository;
 
-    public JobContracted save(JobContracted entity){
-
+    public JobContracted save(JobContracted entity) {
         return jobContractedRepository.save(entity);
     }
 
-    public void delete(Long id){
-
+    public void delete(Long id) {
         jobContractedRepository.deleteById(id);
     }
 
-    public List<JobContracted> findAll(){
-
+    public List<JobContracted> findAll() {
         return this.jobContractedRepository.findAll();
     }
 
-    public List<JobContracted> findByIdProfessional(Long id){
-
+    public List<JobContracted> findByIdProfessional(Long id) {
         return this.jobContractedRepository.findByIdProfessional(id);
     }
+
+    public Integer countByIdProfessional(Long id) {
+        return this.jobContractedRepository.countByIdProfessional(id);
+    }
+
+    public Integer countRatingByIdProfessional(Long id) {
+        return this.jobContractedRepository.countRatingByIdProfessional(id);
+    }
+
+    public Integer countCommentsByIdProfessional(Long id) {
+        return this.jobContractedRepository.countCommentsByIdProfessional(id);
+    }
+
 }
