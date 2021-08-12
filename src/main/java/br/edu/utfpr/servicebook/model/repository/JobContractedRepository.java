78cd6d7196977b2b70ae3851221952ee613d6d84
André <andredesouzaaranda@gmail.com>
2021-08-12 20:43:05 -0300

    Adicionado queries para JobContracted
    Contabilização de serviços, avaliações e comentários

--- src/main/java/br/edu/utfpr/servicebook/model/repository/JobContractedRepository.java
+++ src/main/java/br/edu/utfpr/servicebook/model/repository/JobContractedRepository.java
@@ -12,4 +12,13 @@ public interface JobContractedRepository extends JpaRepository<JobContracted, Lo
     @Query("SELECT j FROM JobContracted j WHERE j.professional.id = :professional_id")
     List<JobContracted> findByIdProfessional(@Param("professional_id") Long professional_id);
 
+    @Query("SELECT COUNT(j) FROM JobContracted j WHERE j.professional.id = :professional_id")
+    Integer countByIdProfessional(@Param("professional_id") Long professional_id);
+
+    @Query("SELECT COUNT(j.rating) FROM JobContracted j WHERE j.professional.id = :professional_id AND j.rating > 0")
+    Integer countRatingByIdProfessional(@Param("professional_id") Long professional_id);
+
+    @Query("SELECT COUNT(j.comments) FROM JobContracted j WHERE j.professional.id = :professional_id AND j.comments <> ''")
+    Integer countCommentsByIdProfessional(@Param("professional_id") Long professional_id);
+
 }
