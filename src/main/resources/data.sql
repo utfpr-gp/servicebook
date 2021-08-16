b7a8826b70659ce1cdfb9ac962862a8c9029bef8
André <andredesouzaaranda@gmail.com>
2021-08-15 23:00:49 -0300

    Atualização data.sql
    Correções

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -29,10 +29,12 @@ VALUES ('Acre', 'AC'),
 
 
 
-INSERT INTO users (birth_date, email, email_verified, gender, name, phone_number, phone_verified, profile_picture, type)
+INSERT INTO users (birth_date, email, email_verified, gender, name, phone_number, phone_verified, profile_picture,
+                   profile_verified, type)
 VALUES ('2018-12-03', 'darth_vader@gmail.com', 'true', 'Masculino', 'Darth Vader', '(42)9 9999-9999', 'true',
-        'https://i.imgur.com/owhNAKK.png', 'profissional'),
-       ('2018-12-03', 'chewbacca@gmail.com', 'false', 'Masculino', 'Chewbacca', '', 'false', '', 'profissional');
+        'https://i.imgur.com/owhNAKK.png', 'true', 'profissional'),
+       ('2018-12-03', 'chewbacca@gmail.com', 'false', 'Masculino', 'Chewbacca', '', 'false', '', 'true',
+        'profissional');
 
 INSERT INTO professionals (cpf, description, rating, denounce_amount, id)
 VALUES ('222222222', 'Sua falta de fé é perturbadora.', '5', '1', '1'),
@@ -56,7 +58,8 @@ VALUES (1, 1, 1),
        (4, 1, 4),
        (5, 1, 5);
 
-INSERT INTO job_requests (client_confirmation, date_created, date_expired, description, professional_confirmation, quantity_candidators_max, status, client_id, expertise_id)
+INSERT INTO job_requests (client_confirmation, date_created, date_expired, description, professional_confirmation,
+                          quantity_candidators_max, status, client_id, expertise_id)
 VALUES ('true', '2018-12-03', '2018-12-03', 'Lorem ipsum dolor', 'true', '3', 'CLOSED', '2', '1'),
        ('true', '2018-12-03', '2018-12-03', 'Lorem ipsum dolor', 'true', '4', 'CLOSED', '2', '1'),
        ('true', '2018-12-03', '2018-12-03', 'Lorem ipsum dolor', 'true', '3', 'CLOSED', '2', '2');
