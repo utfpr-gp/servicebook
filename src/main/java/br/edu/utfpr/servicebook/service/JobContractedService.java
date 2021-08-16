8714e821302a408528ffe192909422d083c6d868
André <andredesouzaaranda@gmail.com>
2021-08-15 22:54:05 -0300

    Atualização JobContractedService
    Correções nos métodos de serviços

--- src/main/java/br/edu/utfpr/servicebook/service/JobContractedService.java
+++ src/main/java/br/edu/utfpr/servicebook/service/JobContractedService.java
@@ -1,11 +1,15 @@
 package br.edu.utfpr.servicebook.service;
 
+import br.edu.utfpr.servicebook.model.entity.Expertise;
 import br.edu.utfpr.servicebook.model.entity.JobContracted;
+import br.edu.utfpr.servicebook.model.entity.Professional;
 import br.edu.utfpr.servicebook.model.repository.JobContractedRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.swing.text.html.Option;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class JobContractedService {
@@ -29,16 +33,28 @@ public class JobContractedService {
         return this.jobContractedRepository.findByIdProfessional(id);
     }
 
-    public Integer countByIdProfessional(Long id) {
-        return this.jobContractedRepository.countByIdProfessional(id);
+    public Optional<Long> countByProfessional(Professional professional) {
+        return this.jobContractedRepository.countByProfessional(professional);
     }
 
-    public Integer countRatingByIdProfessional(Long id) {
-        return this.jobContractedRepository.countRatingByIdProfessional(id);
+    public Optional<Long> countRatingByProfessional(Professional professional) {
+        return this.jobContractedRepository.countRatingByProfessional(professional);
     }
 
-    public Integer countCommentsByIdProfessional(Long id) {
-        return this.jobContractedRepository.countCommentsByIdProfessional(id);
+    public Optional<Long> countCommentsByProfessional(Professional professional) {
+        return this.jobContractedRepository.countCommentsByProfessional(professional);
+    }
+
+    public Optional<Long> countByProfessionalAndJobRequest_Expertise(Professional professional, Expertise expertise) {
+        return this.jobContractedRepository.countByProfessionalAndJobRequest_Expertise(professional, expertise);
+    }
+
+    public Optional<Long> countRatingByProfessionalAndJobRequest_Expertise(Professional professional, Expertise expertise) {
+        return this.jobContractedRepository.countRatingByProfessionalAndJobRequest_Expertise(professional, expertise);
+    }
+
+    public Optional<Long> countCommentsByProfessionalAndJobRequest_Expertise(Professional professional, Expertise expertise) {
+        return this.jobContractedRepository.countCommentsByProfessionalAndJobRequest_Expertise(professional, expertise);
     }
 
 }
