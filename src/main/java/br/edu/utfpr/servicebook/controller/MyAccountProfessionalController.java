ee1e4f3a8122572baefcd578e83394702551bc3e
André <andredesouzaaranda@gmail.com>
2021-08-15 22:37:04 -0300

    Atualização MyAccountProfessionalController
    Correções da minha conta profissional

--- src/main/java/br/edu/utfpr/servicebook/controller/MyAccountProfessionalController.java
+++ src/main/java/br/edu/utfpr/servicebook/controller/MyAccountProfessionalController.java
@@ -1,12 +1,15 @@
 package br.edu.utfpr.servicebook.controller;
 
+import br.edu.utfpr.servicebook.exception.InvalidParamsException;
 import br.edu.utfpr.servicebook.model.dto.ExpertiseDTO;
 import br.edu.utfpr.servicebook.model.dto.ProfessionalMinDTO;
+import br.edu.utfpr.servicebook.model.entity.Expertise;
 import br.edu.utfpr.servicebook.model.entity.Professional;
 import br.edu.utfpr.servicebook.model.entity.ProfessionalExpertise;
 import br.edu.utfpr.servicebook.model.mapper.ExpertiseMapper;
 import br.edu.utfpr.servicebook.model.mapper.JobContractedMapper;
 import br.edu.utfpr.servicebook.model.mapper.ProfessionalMapper;
+import br.edu.utfpr.servicebook.service.ExpertiseService;
 import br.edu.utfpr.servicebook.service.JobContractedService;
 import br.edu.utfpr.servicebook.service.ProfessionalExpertiseService;
 import br.edu.utfpr.servicebook.service.ProfessionalService;
@@ -16,19 +19,20 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
 @RequestMapping("/minha-conta")
 @Controller
-public class ProfessionalController {
+public class MyAccountProfessionalController {
 
-    public static final Logger log = LoggerFactory.getLogger(ProfessionalController.class);
+    public static final Logger log = LoggerFactory.getLogger(MyAccountProfessionalController.class);
 
     @Autowired
     private ProfessionalService professionalService;
@@ -40,6 +44,9 @@ public class ProfessionalController {
     private ProfessionalExpertiseService professionalExpertiseService;
 
     @Autowired
+    private ExpertiseService expertiseService;
+
+    @Autowired
     private ExpertiseMapper expertiseMapper;
 
     @Autowired
@@ -49,41 +56,55 @@ public class ProfessionalController {
     private JobContractedMapper jobContractedMapper;
 
     @GetMapping
-    public ModelAndView showMyAccount() {
+    public ModelAndView showMyAccount(@RequestParam Optional<Long> id) {
         log.debug("ServiceBook: Minha conta.");
 
-        ModelAndView mv = new ModelAndView("professional/my-account");
-
         Optional<Professional> oProfessional = Optional.ofNullable(professionalService.findByEmailAddress(CurrentUserUtil.getCurrentUserEmail()));
-
         ProfessionalMinDTO professionalMinDTO = professionalMapper.toMinDto(oProfessional.get());
-        mv.addObject("professional", professionalMinDTO);
 
         List<ProfessionalExpertise> professionalExpertises = professionalExpertiseService.findByProfessional(oProfessional.get());
         List<ExpertiseDTO> expertiseDTOs = professionalExpertises.stream()
                 .map(professionalExpertise -> professionalExpertise.getExpertise())
                 .map(expertise -> expertiseMapper.toDto(expertise))
                 .collect(Collectors.toList());
-        mv.addObject("expertises", expertiseDTOs);
-
-        Integer jobs = jobContractedService.countByIdProfessional(oProfessional.get().getId());
-        mv.addObject("jobs", jobs);
-
-        Integer ratings = jobContractedService.countRatingByIdProfessional(oProfessional.get().getId());
-        mv.addObject("ratings", ratings);
-
-        Integer comments = jobContractedService.countCommentsByIdProfessional(oProfessional.get().getId());
-        mv.addObject("comments", comments);
-
-        return mv;
-    }
-
-    @GetMapping("/filtros/{expertise}")
-    public ModelAndView showStatusByExpertise(@PathVariable("expertise") Long expertise) {
-        log.debug("ServiceBook: Minha conta - Especialidades.");
 
         ModelAndView mv = new ModelAndView("professional/my-account");
-        mv.addObject("expertise", expertise);
+
+        mv.addObject("professional", professionalMinDTO);
+        mv.addObject("expertises", expertiseDTOs);
+
+        Optional<Long> jobs, ratings, comments;
+
+        if (!id.isPresent() || id.get() == 0L) {
+            jobs = jobContractedService.countByProfessional(oProfessional.get());
+            comments = jobContractedService.countCommentsByProfessional(oProfessional.get());
+            ratings = jobContractedService.countRatingByProfessional(oProfessional.get());
+
+            mv.addObject("id", 0L);
+        } else {
+            if (id.get() < 0) {
+                throw new InvalidParamsException("O id informado não pode ser negativo.");
+            }
+
+            Optional<Expertise> expertise = expertiseService.findById(id.get());
+
+            if (!expertise.isPresent()) {
+                throw new EntityNotFoundException("A especialidade não foi encontrada pelo id informado.");
+            }
+
+            Optional<Integer> professionalExpertiseRating = professionalExpertiseService.selectRatingByProfessionalAndExpertise(oProfessional.get().getId(), expertise.get().getId());
+
+            jobs = jobContractedService.countByProfessionalAndJobRequest_Expertise(oProfessional.get(), expertise.get());
+            comments = jobContractedService.countCommentsByProfessionalAndJobRequest_Expertise(oProfessional.get(), expertise.get());
+            ratings = jobContractedService.countRatingByProfessionalAndJobRequest_Expertise(oProfessional.get(), expertise.get());
+
+            mv.addObject("id", id.get());
+            mv.addObject("professionalExpertiseRating", professionalExpertiseRating.get());
+        }
+
+        mv.addObject("jobs", jobs.get());
+        mv.addObject("ratings", ratings.get());
+        mv.addObject("comments", comments.get());
 
         return mv;
     }
