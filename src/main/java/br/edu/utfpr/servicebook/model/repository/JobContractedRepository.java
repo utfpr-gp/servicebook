1733ead3dafbc424a36381a261ca2409e1484104
André <andredesouzaaranda@gmail.com>
2021-08-15 22:44:20 -0300

    Atualização JobContractedRepository
    Correções para uso do Spring Data JPA

--- src/main/java/br/edu/utfpr/servicebook/model/repository/JobContractedRepository.java
+++ src/main/java/br/edu/utfpr/servicebook/model/repository/JobContractedRepository.java
@@ -1,24 +1,69 @@
 package br.edu.utfpr.servicebook.model.repository;
 
+import br.edu.utfpr.servicebook.model.entity.Expertise;
 import br.edu.utfpr.servicebook.model.entity.JobContracted;
+import br.edu.utfpr.servicebook.model.entity.Professional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface JobContractedRepository extends JpaRepository<JobContracted, Long> {
 
     @Query("SELECT j FROM JobContracted j WHERE j.professional.id = :professional_id")
     List<JobContracted> findByIdProfessional(@Param("professional_id") Long professional_id);
 
-    @Query("SELECT COUNT(j) FROM JobContracted j WHERE j.professional.id = :professional_id")
-    Integer countByIdProfessional(@Param("professional_id") Long professional_id);
+    /**
+     * Retorna o total de trabalhos contratados de um profissional.
+     *
+     * @param professional
+     * @return Optional<Long>
+     */
+    Optional<Long> countByProfessional(Professional professional);
 
-    @Query("SELECT COUNT(j.rating) FROM JobContracted j WHERE j.professional.id = :professional_id AND j.rating > 0")
-    Integer countRatingByIdProfessional(@Param("professional_id") Long professional_id);
+    /**
+     * Retorna o total de avaliações dos trabalhos contratados de um profissional.
+     *
+     * @param professional
+     * @return Optional<Long>
+     */
+    Optional<Long> countRatingByProfessional(Professional professional);
 
-    @Query("SELECT COUNT(j.comments) FROM JobContracted j WHERE j.professional.id = :professional_id AND j.comments <> ''")
-    Integer countCommentsByIdProfessional(@Param("professional_id") Long professional_id);
+    /**
+     * Retorna o total de comentários dos trabalhos contratados de um profissional.
+     *
+     * @param professional
+     * @return Optional<Long>
+     */
+    Optional<Long> countCommentsByProfessional(Professional professional);
+
+    /**
+     * Retorna o total de trabalhos contratados de um profissional para uma dada especialidade.
+     *
+     * @param professional
+     * @param expertise
+     * @return Optional<Long>
+     */
+    Optional<Long> countByProfessionalAndJobRequest_Expertise(Professional professional, Expertise expertise);
+
+    /**
+     * Retorna o total de avaliações dos trabalhos contratados de um profissional para uma dada especialidade.
+     *
+     * @param professional
+     * @param expertise
+     * @return Optional<Long>
+     */
+    Optional<Long> countRatingByProfessionalAndJobRequest_Expertise(Professional professional, Expertise expertise);
+
+    /**
+     * Retorna o total de comentários dos trabalhos contratados de um profissional para uma dada especialidade.
+     *
+     * @param professional
+     * @param expertise
+     * @return Optional<Long>
+     */
+    Optional<Long> countCommentsByProfessionalAndJobRequest_Expertise(Professional professional, Expertise expertise);
 
 }
