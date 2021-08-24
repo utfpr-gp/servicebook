01d4a4485cfd9292ec7dd2e774fe79ac5de14d00
Roni Fabio Banaszewski <roni.banaszewski@gmail.com>
2021-08-24 12:29:11 -0300

    Refatorada a inserção inicial no BD
    Substituição da inserção de dados via data.sql por service em Java

---
+++ src/main/java/br/edu/utfpr/servicebook/service/IndexService.java
@@ -0,0 +1,43 @@
+package br.edu.utfpr.servicebook.service;
+
+import br.edu.utfpr.servicebook.model.entity.City;
+import br.edu.utfpr.servicebook.model.entity.State;
+import br.edu.utfpr.servicebook.model.repository.CityRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CityService {
+    @Autowired
+    private CityRepository cityRepository;
+
+    public City save(City entity){
+        return cityRepository.save(entity);
+    }
+
+    public void delete(Long id){
+        cityRepository.deleteById(id);
+    }
+
+    public List<City> findAll(){
+        return this.cityRepository.findAll();
+    }
+
+    public Page<City> findAll(PageRequest pageRequest){
+        return this.cityRepository.findAll(pageRequest);
+    }
+
+    public Optional<City> findById(Long id){
+        return this.cityRepository.findById(id);
+    }
+
+    public Optional<City> findByNameAndState(String name, State state){
+        return this.cityRepository.findByNameAndState(name, state);
+    }
+
+}
