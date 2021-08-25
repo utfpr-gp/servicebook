bdc5ce53db71010b1a208d331a7202083cbbf5c3
André <andredesouzaaranda@gmail.com>
2021-08-25 19:58:48 -0300

    Adicionado CityMinDTO

---
+++ src/main/java/br/edu/utfpr/servicebook/model/dto/CityMinDTO.java
@@ -0,0 +1,19 @@
+package br.edu.utfpr.servicebook.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class CityMinDTO implements Serializable {
+
+    private String name;
+    private String image;
+    private StateMinDTO state;
+
+}
