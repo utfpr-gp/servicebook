7aa9e1490e673ed5efbfbf84157c82a5f7a11449
André <andredesouzaaranda@gmail.com>
2021-08-25 19:56:15 -0300

    Adicionado JobCandidateMapper

---
+++ src/main/java/br/edu/utfpr/servicebook/model/mapper/JobCandidateMapper.java
@@ -0,0 +1,30 @@
+package br.edu.utfpr.servicebook.model.mapper;
+
+
+import br.edu.utfpr.servicebook.model.dto.JobCandidateDTO;
+import br.edu.utfpr.servicebook.model.entity.JobCandidate;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+
+import java.text.SimpleDateFormat;
+
+@Component
+public class JobCandidateMapper {
+
+    private static final SimpleDateFormat dateFormat = new SimpleDateFormat("dd/MM/yyyy");
+
+    @Autowired
+    private ModelMapper mapper;
+
+    public JobCandidateDTO toDto(JobCandidate entity) {
+        JobCandidateDTO dto = mapper.map(entity, JobCandidateDTO.class);
+        return dto;
+    }
+
+    public JobCandidate toEntity(JobCandidateDTO dto) {
+        JobCandidate entity = mapper.map(dto, JobCandidate.class);
+        return entity;
+    }
+
+}
