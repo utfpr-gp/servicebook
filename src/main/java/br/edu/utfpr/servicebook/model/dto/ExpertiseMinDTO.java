4b1757536be9b7deb2afe113c0190a582273e763
André <andredesouzaaranda@gmail.com>
2021-08-25 20:00:00 -0300

    Adicionado ExpertiseMinDTO

---
+++ src/main/java/br/edu/utfpr/servicebook/model/dto/ExpertiseMinDTO.java
@@ -0,0 +1,16 @@
+package br.edu.utfpr.servicebook.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ExpertiseMinDTO implements Serializable {
+
+    private String name;
+
+}
