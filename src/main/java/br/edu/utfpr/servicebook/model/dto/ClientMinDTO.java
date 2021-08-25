713d280748a01bce5cbf9450ca619e0d5a596aba
André <andredesouzaaranda@gmail.com>
2021-08-25 19:59:15 -0300

    Adicionado ClientMinDTO

---
+++ src/main/java/br/edu/utfpr/servicebook/model/dto/ClientMinDTO.java
@@ -0,0 +1,18 @@
+package br.edu.utfpr.servicebook.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class ClientMinDTO implements Serializable {
+
+    private String name;
+    private String profilePicture;
+    private AddressMinDTO address;
+
+}
