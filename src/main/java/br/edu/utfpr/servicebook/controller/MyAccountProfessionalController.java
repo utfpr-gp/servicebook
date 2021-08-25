e21ba58954a85de3bcc802fd9a73125bf0b9384e
André <andredesouzaaranda@gmail.com>
2021-08-25 19:39:14 -0300

    Atualização MyAccountProfessionalController
    Adicionado rotas AJAX

--- src/main/java/br/edu/utfpr/servicebook/controller/MyAccountProfessionalController.java
+++ src/main/java/br/edu/utfpr/servicebook/controller/MyAccountProfessionalController.java
@@ -1,22 +1,19 @@
 package br.edu.utfpr.servicebook.controller;
 
 import br.edu.utfpr.servicebook.exception.InvalidParamsException;
-import br.edu.utfpr.servicebook.model.dto.ExpertiseDTO;
-import br.edu.utfpr.servicebook.model.dto.ProfessionalMinDTO;
-import br.edu.utfpr.servicebook.model.entity.Expertise;
-import br.edu.utfpr.servicebook.model.entity.Professional;
-import br.edu.utfpr.servicebook.model.entity.ProfessionalExpertise;
-import br.edu.utfpr.servicebook.model.mapper.ExpertiseMapper;
-import br.edu.utfpr.servicebook.model.mapper.JobContractedMapper;
-import br.edu.utfpr.servicebook.model.mapper.ProfessionalMapper;
-import br.edu.utfpr.servicebook.service.ExpertiseService;
-import br.edu.utfpr.servicebook.service.JobContractedService;
-import br.edu.utfpr.servicebook.service.ProfessionalExpertiseService;
-import br.edu.utfpr.servicebook.service.ProfessionalService;
+import br.edu.utfpr.servicebook.model.dto.*;
+import br.edu.utfpr.servicebook.model.entity.*;
+import br.edu.utfpr.servicebook.model.mapper.*;
+import br.edu.utfpr.servicebook.service.*;
 import br.edu.utfpr.servicebook.util.CurrentUserUtil;
+import br.edu.utfpr.servicebook.util.pagination.PaginationDTO;
+import br.edu.utfpr.servicebook.util.pagination.PaginationUtil;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -24,11 +21,12 @@ import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import javax.persistence.EntityNotFoundException;
+import javax.servlet.http.HttpServletRequest;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
 
-@RequestMapping("/minha-conta")
+@RequestMapping("/minha-conta/profissional")
 @Controller
 public class MyAccountProfessionalController {
 
@@ -55,8 +53,20 @@ public class MyAccountProfessionalController {
     @Autowired
     private JobContractedMapper jobContractedMapper;
 
+    @Autowired
+    private JobRequestService jobRequestService;
+
+    @Autowired
+    private JobRequestMapper jobRequestMapper;
+
+    @Autowired
+    private JobCandidateService jobCandidateService;
+
+    @Autowired
+    private JobCandidateMapper jobCandidateMapper;
+
     @GetMapping
-    public ModelAndView showMyAccount(@RequestParam Optional<Long> id) throws Exception {
+    public ModelAndView showMyAccountProfessional(@RequestParam(required = false, defaultValue = "0") Optional<Long> id) throws Exception {
         log.debug("ServiceBook: Minha conta.");
 
         Optional<Professional> oProfessional = Optional.ofNullable(professionalService.findByEmailAddress(CurrentUserUtil.getCurrentUserEmail()));
@@ -116,6 +126,141 @@ public class MyAccountProfessionalController {
         mv.addObject("jobs", jobs.get());
         mv.addObject("ratings", ratings.get());
         mv.addObject("comments", comments.get());
+
+        return mv;
+    }
+
+    @GetMapping("/disponiveis")
+    public ModelAndView showAvailableJobs(
+            HttpServletRequest request,
+            @RequestParam(required = false, defaultValue = "0") Long id,
+            @RequestParam(value = "pag", defaultValue = "1") int page,
+            @RequestParam(value = "siz", defaultValue = "3") int size,
+            @RequestParam(value = "ord", defaultValue = "id") String order,
+            @RequestParam(value = "dir", defaultValue = "ASC") String direction
+    ) throws Exception {
+
+        Optional<Professional> oProfessional = Optional.ofNullable(professionalService.findByEmailAddress(CurrentUserUtil.getCurrentUserEmail()));
+
+        if (!oProfessional.isPresent()) {
+            throw new Exception("Usuário não autenticado! Por favor, realize sua autenticação no sistema.");
+        }
+
+        PageRequest pageRequest = PageRequest.of(page - 1, size, Sort.by("dateExpired").ascending());
+        Page<JobRequest> jobRequestPage = null;
+        List<JobRequestFullDTO> jobRequestFullDTOs = null;
+
+        if (id == 0) {
+            jobRequestPage = jobRequestService.findByStatusAndJobCandidatesIsNullOrJobCandidates_ProfessionalNot(JobRequest.Status.AVAILABLE, oProfessional.get(), pageRequest);
+        } else {
+            if (id < 0) {
+                throw new InvalidParamsException("O identificador da especialidade não pode ser negativo. Por favor, tente novamente.");
+            }
+
+            Optional<Expertise> oExpertise = expertiseService.findById(id);
+
+            if (!oExpertise.isPresent()) {
+                throw new EntityNotFoundException("A especialidade não foi encontrada pelo id informado. Por favor, tente novamente.");
+            }
+
+            Optional<ProfessionalExpertise> oProfessionalExpertise = professionalExpertiseService.findByProfessionalAndExpertise(oProfessional.get(), oExpertise.get());
+
+            if (!oProfessionalExpertise.isPresent()) {
+                throw new InvalidParamsException("A especialidade profissional não foi encontrada. Por favor, tente novamente.");
+            }
+
+            jobRequestPage = jobRequestService.findByStatusAndExpertiseAndJobCandidatesIsNullOrJobCandidates_ProfessionalNot(JobRequest.Status.AVAILABLE, oExpertise.get(), oProfessional.get(), pageRequest);
+        }
+
+        jobRequestFullDTOs = jobRequestPage.stream()
+                .map(jobRequest -> {
+                    Optional<Long> totalCandidates = jobCandidateService.countByJobRequest(jobRequest);
+
+                    if (totalCandidates.isPresent()) {
+                        return jobRequestMapper.toFullDto(jobRequest, totalCandidates);
+                    }
+
+                    return jobRequestMapper.toFullDto(jobRequest, Optional.ofNullable(0L));
+                }).collect(Collectors.toList());
+
+        PaginationDTO paginationDTO = PaginationUtil.getPaginationDTO(jobRequestPage, "/minha-conta/profissional/disponiveis");
+
+        ModelAndView mv = new ModelAndView("professional/available-jobs-report");
+        mv.addObject("pagination", paginationDTO);
+        mv.addObject("jobs", jobRequestFullDTOs);
+
+        return mv;
+    }
+
+    ////// --->
+    @GetMapping("/em-disputa")
+    public ModelAndView showDisputedJobs(
+            HttpServletRequest request,
+            @RequestParam(required = false, defaultValue = "0") Long id,
+            @RequestParam(value = "pag", defaultValue = "1") int page,
+            @RequestParam(value = "siz", defaultValue = "3") int size,
+            @RequestParam(value = "ord", defaultValue = "id") String order,
+            @RequestParam(value = "dir", defaultValue = "ASC") String direction
+    ) throws Exception {
+
+        Optional<Professional> oProfessional = Optional.ofNullable(professionalService.findByEmailAddress(CurrentUserUtil.getCurrentUserEmail()));
+
+        if (!oProfessional.isPresent()) {
+            throw new Exception("Usuário não autenticado! Por favor, realize sua autenticação no sistema.");
+        }
+
+        PageRequest pageRequest = PageRequest.of(page - 1, size, Sort.by("id").ascending());
+        Page<JobCandidate> jobRequestPage = null;
+        List<JobCandidateDTO> jobCandidateDTOs = null;
+
+        if (id == 0) {
+            jobRequestPage = jobCandidateService.findByProfessional(oProfessional.get(), pageRequest);
+
+            jobCandidateDTOs = jobRequestPage.stream()
+                    .map(jobCandidate -> jobCandidateMapper.toDto(jobCandidate))
+                    .collect(Collectors.toList());
+        }
+
+        PaginationDTO paginationDTO = PaginationUtil.getPaginationDTO(jobRequestPage, "/minha-conta/profissional/disponiveis");
+
+        ModelAndView mv = new ModelAndView("professional/disputed-jobs-report");
+        mv.addObject("pagination", paginationDTO);
+        mv.addObject("jobs", jobCandidateDTOs);
+
+        return mv;
+    }
+
+    @GetMapping("/para-fazer")
+    public ModelAndView showJobsTodo() {
+        ModelAndView mv = new ModelAndView("available-jobs-report");
+        return mv;
+    }
+
+    @GetMapping("/executados")
+    public ModelAndView showJobsPerformed() {
+        ModelAndView mv = new ModelAndView("available-jobs-report");
+        return mv;
+    }
+
+    @GetMapping("/detalhes")
+    public ModelAndView showMyDetails() throws Exception {
+        ModelAndView mv = new ModelAndView("client/details-contact");
+
+        Optional<Professional> oProfessional = Optional.ofNullable(professionalService.findByEmailAddress(CurrentUserUtil.getCurrentUserEmail()));
+
+        if (!oProfessional.isPresent()) {
+            throw new Exception("Usuário não autenticado! Por favor, realize sua autenticação no sistema.");
+        }
+
+        ProfessionalDTO professionalDTO = professionalMapper.toResponseDto(oProfessional.get());
+
+        List<JobContracted> jobContracted = jobContractedService.findByIdProfessional(professionalDTO.getId());
+        List<JobContractedDTO> jobContractedDTOs = jobContracted.stream()
+                .map(contracted -> jobContractedMapper.toResponseDto(contracted))
+                .collect(Collectors.toList());
+
+        mv.addObject("professional", professionalDTO);
+        mv.addObject("jobContracted", jobContractedDTOs);
 
         return mv;
     }
