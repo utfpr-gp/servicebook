05f7e735536dedaba6ffa0ad2c88e4b207fd085d
André <andredesouzaaranda@gmail.com>
2021-08-26 14:09:44 -0300

    Atualização JobCandidateMapper
    Adicionado Mapper para vinculação de dados entre JobCandidate e JobRequest

--- src/main/java/br/edu/utfpr/servicebook/model/mapper/JobCandidateMapper.java
+++ src/main/java/br/edu/utfpr/servicebook/model/mapper/JobCandidateMapper.java
@@ -1,13 +1,15 @@
 package br.edu.utfpr.servicebook.model.mapper;
 
 
-import br.edu.utfpr.servicebook.model.dto.JobCandidateDTO;
+import br.edu.utfpr.servicebook.model.dto.JobCandidateMinDTO;
 import br.edu.utfpr.servicebook.model.entity.JobCandidate;
+import br.edu.utfpr.servicebook.util.DateUtil;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import java.text.SimpleDateFormat;
+import java.util.Optional;
 
 @Component
 public class JobCandidateMapper {
@@ -17,14 +19,17 @@ public class JobCandidateMapper {
     @Autowired
     private ModelMapper mapper;
 
-    public JobCandidateDTO toDto(JobCandidate entity) {
-        JobCandidateDTO dto = mapper.map(entity, JobCandidateDTO.class);
-        return dto;
-    }
+    public JobCandidateMinDTO toMinDto(JobCandidate entity, Optional<Long> totalCandidates) {
+        JobCandidateMinDTO dto = mapper.map(entity, JobCandidateMinDTO.class);
+        dto.getJobRequest().setDateCreated(this.dateFormat.format(entity.getJobRequest().getDateCreated()));
+        dto.getJobRequest().setDateExpired(this.dateFormat.format(entity.getJobRequest().getDateExpired()));
+        dto.setDate(this.dateFormat.format(entity.getDate()));
+        dto.getJobRequest().setTotalCandidates(totalCandidates.get());
 
-    public JobCandidate toEntity(JobCandidateDTO dto) {
-        JobCandidate entity = mapper.map(dto, JobCandidate.class);
-        return entity;
+        Long intervalOfDays = DateUtil.getDifferenceInDays(entity.getJobRequest().getDateCreated(), entity.getJobRequest().getDateExpired());
+        dto.getJobRequest().setIntervalOfDays(intervalOfDays);
+
+        return dto;
     }
 
 }
