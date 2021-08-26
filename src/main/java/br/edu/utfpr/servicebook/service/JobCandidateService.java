40cdd7b8e2830a4421b600237145f9dec105ee39
André <andredesouzaaranda@gmail.com>
2021-08-26 14:07:38 -0300

    Atualização JobCandidateService
    Adicionado serviços de buscas por candidaturas de profissional com paginação

--- src/main/java/br/edu/utfpr/servicebook/service/JobCandidateService.java
+++ src/main/java/br/edu/utfpr/servicebook/service/JobCandidateService.java
@@ -1,25 +1,21 @@
-
 package br.edu.utfpr.servicebook.service;
 
-        import br.edu.utfpr.servicebook.model.entity.Client;
-        import br.edu.utfpr.servicebook.model.entity.JobCandidate;
-        import br.edu.utfpr.servicebook.model.entity.JobRequest;
-        import br.edu.utfpr.servicebook.model.entity.Professional;
-        import br.edu.utfpr.servicebook.model.repository.ClientRepository;
-        import br.edu.utfpr.servicebook.model.repository.JobCandidateRepository;
-        import lombok.extern.slf4j.Slf4j;
-        import org.springframework.beans.factory.annotation.Autowired;
-        import org.springframework.data.domain.Page;
-        import org.springframework.data.domain.Pageable;
-        import org.springframework.stereotype.Service;
+import br.edu.utfpr.servicebook.model.entity.Expertise;
+import br.edu.utfpr.servicebook.model.entity.JobCandidate;
+import br.edu.utfpr.servicebook.model.entity.JobRequest;
+import br.edu.utfpr.servicebook.model.entity.Professional;
+import br.edu.utfpr.servicebook.model.repository.JobCandidateRepository;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
 
-        import java.util.List;
-        import java.util.Optional;
+import java.util.Optional;
 
 @Slf4j
 @Service
 public class JobCandidateService {
-
 
     @Autowired
     private JobCandidateRepository jobCandidateRepository;
@@ -28,11 +24,12 @@ public class JobCandidateService {
         return this.jobCandidateRepository.countByJobRequest(jobRequest);
     }
 
-    public Page<JobCandidate> findByProfessional(Professional professional, Pageable pageable){
-        return this.jobCandidateRepository.findByProfessional(professional, pageable);
+    public Page<JobCandidate> findByJobRequest_StatusAndProfessional(JobRequest.Status status, Professional professional, Pageable pageable) {
+        return this.jobCandidateRepository.findByJobRequest_StatusAndProfessional(status, professional, pageable);
     }
 
-    public void init() {
-
+    public Page<JobCandidate> findByJobRequest_StatusAndJobRequest_ExpertiseAndProfessional(JobRequest.Status status, Expertise expertise, Professional professional, Pageable pageable) {
+        return this.jobCandidateRepository.findByJobRequest_StatusAndJobRequest_ExpertiseAndProfessional(status, expertise, professional, pageable);
     }
+
 }
