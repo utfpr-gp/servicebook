fa839ed23d80d1b89fa7cdbdb474f8630506490f
Roni Fabio Banaszewski <roni.banaszewski@gmail.com>
2021-08-27 20:43:16 -0300

    Adicionado importações

--- src/main/java/br/edu/utfpr/servicebook/service/JobCandidateService.java
+++ src/main/java/br/edu/utfpr/servicebook/service/JobCandidateService.java
@@ -11,6 +11,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Slf4j
