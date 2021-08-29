1ded3665f311613e0094fc7fad9e4c38ea9a2906
Amanda <amandakarolinecs@gmail.com>
2021-08-28 23:36:22 -0300

    Atualização JobCandidateService

--- src/main/java/br/edu/utfpr/servicebook/service/JobCandidateService.java
+++ src/main/java/br/edu/utfpr/servicebook/service/JobCandidateService.java
@@ -1,9 +1,6 @@
 package br.edu.utfpr.servicebook.service;
 
-import br.edu.utfpr.servicebook.model.entity.Expertise;
-import br.edu.utfpr.servicebook.model.entity.JobCandidate;
-import br.edu.utfpr.servicebook.model.entity.JobRequest;
-import br.edu.utfpr.servicebook.model.entity.Professional;
+import br.edu.utfpr.servicebook.model.entity.*;
 import br.edu.utfpr.servicebook.model.repository.JobCandidateRepository;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,6 +8,7 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Slf4j
@@ -32,6 +30,9 @@ public class JobCandidateService {
         return this.jobCandidateRepository.findByJobRequest(jobRequest);
     }
 
+    public Page<JobCandidate> findByJobRequest_StatusAndJobRequest_Client(JobRequest.Status status, Client client , Pageable pageable) {
+        return this.jobCandidateRepository.findByJobRequest_StatusAndJobRequest_Client(status, client, pageable);
+    }
 
     public Page<JobCandidate> findByJobRequest_StatusAndJobRequest_ExpertiseAndProfessional(JobRequest.Status status, Expertise expertise, Professional professional, Pageable pageable) {
         return this.jobCandidateRepository.findByJobRequest_StatusAndJobRequest_ExpertiseAndProfessional(status, expertise, professional, pageable);
