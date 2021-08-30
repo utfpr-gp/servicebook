dbddbbe144a68329f6bef0287021ad747f15c587
André <andredesouzaaranda@gmail.com>
2021-08-30 16:13:58 -0300

    Adicionado UserCodeRepository
    Adicionado queries de buscas por email e código

---
+++ src/main/java/br/edu/utfpr/servicebook/model/repository/UserCodeRepository.java
@@ -0,0 +1,16 @@
+package br.edu.utfpr.servicebook.model.repository;
+
+import br.edu.utfpr.servicebook.model.entity.UserCode;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface UserCodeRepository extends JpaRepository<UserCode, Long> {
+
+    Optional<UserCode> findByEmail(String email);
+
+    Optional<UserCode> findByCode(String code);
+
+    Optional<UserCode> findByEmailAndCode(String email, String code);
+
+}
