c8e3ef96729cd8701145c8e6b4b6ed50931e38a8
André <andredesouzaaranda@gmail.com>
2021-08-30 16:11:11 -0300

    Adicionado UserCodeDTO
    Adicionado validação para código de autenticação

---
+++ src/main/java/br/edu/utfpr/servicebook/model/dto/UserCodeDTO.java
@@ -0,0 +1,26 @@
+package br.edu.utfpr.servicebook.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.validation.constraints.Pattern;
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class UserCodeDTO implements Serializable {
+
+    private String email;
+
+    @Pattern(regexp = "^([0-9a-f]{8}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{4}-[0-9a-f]{12})$", message = "Código inválido! Por favor, insira o código de verificação.", groups = UserCodeDTO.RequestUserCodeInfoGroupValidation.class)
+    private String code;
+
+    public interface RequestUserCodeInfoGroupValidation {
+
+    }
+
+}
