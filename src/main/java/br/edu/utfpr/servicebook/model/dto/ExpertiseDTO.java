e8b18a44c39eb8afabdbd658c87b7a490888d6d9
André <andredesouzaaranda@gmail.com>
2021-08-30 16:05:51 -0300

    Atualização ExpertiseDTO
    Correções

--- src/main/java/br/edu/utfpr/servicebook/model/dto/ExpertiseDTO.java
+++ src/main/java/br/edu/utfpr/servicebook/model/dto/ExpertiseDTO.java
@@ -1,16 +1,22 @@
 package br.edu.utfpr.servicebook.model.dto;
 
-import lombok.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
 
 import javax.validation.constraints.NotBlank;
+import java.io.Serializable;
 
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @ToString
-public class ExpertiseDTO {
+public class ExpertiseDTO implements Serializable {
+
     private Long id;
 
     @NotBlank(message = "O nome da especialidade é obrigatório")
     private String name;
+
 }
