0e47d02b6938838e6a7a7cb833b096a54b02c504
André <andredesouzaaranda@gmail.com>
2021-08-31 21:47:00 -0300

    Atualização UserDTO
    Refatoração

--- src/main/java/br/edu/utfpr/servicebook/model/dto/UserDTO.java
+++ src/main/java/br/edu/utfpr/servicebook/model/dto/UserDTO.java
@@ -6,6 +6,7 @@ import lombok.NoArgsConstructor;
 import lombok.ToString;
 import org.hibernate.validator.constraints.br.CPF;
 
+import javax.validation.Valid;
 import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.Pattern;
@@ -20,12 +21,10 @@ public class UserDTO implements Serializable {
 
     private Long id;
 
-    @NotBlank(message = "Nome inválido! Por favor, insira o nome completo.", groups = UserDTO.RequestUserNameInfoGroupValidation.class)
-    @Pattern(regexp = "^(\\s?[A-ZÀ-Ú][a-zà-ú]*)+(\\s[a-zà-ú]*)?(\\s[A-ZÀ-Ú][a-zà-ú]*)+$", message = "Nome inválido! Por favor, insira o nome completo.", groups = UserDTO.RequestUserNameInfoGroupValidation.class)
+    @Pattern(regexp = "^(\\s?[A-ZÀ-Ú][a-zà-ú]*)+(\\s[a-zà-ú]*)?(\\s[A-ZÀ-Ú][a-zà-ú]*)+$", message = "Nome inválido! Por favor, insira o nome completo.", groups = UserDTO.RequestUserNameAndCPFInfoGroupValidation.class)
     private String name;
 
-    @NotBlank(message = "CPF inválido! Por favor, insira o CPF.", groups = UserDTO.RequestUserCPFInfoGroupValidation.class)
-    @CPF(message = "CPF inválido! Por favor, insira um CPF válido.", groups = UserDTO.RequestUserCPFInfoGroupValidation.class)
+    @CPF(message = "CPF inválido! Por favor, insira um CPF válido.", groups = UserDTO.RequestUserNameAndCPFInfoGroupValidation.class)
     private String cpf;
 
     @NotBlank(message = "Email inválido! Por favor, insira o email.", groups = UserDTO.RequestUserEmailInfoGroupValidation.class)
@@ -37,7 +36,6 @@ public class UserDTO implements Serializable {
     private String profilePicture;
     private Date birthDate;
 
-    @NotBlank(message = "Telefone inválido! Por favor, insira o número de telefone.", groups = UserDTO.RequestUserPhoneInfoGroupValidation.class)
     @Pattern(regexp = "^\\(?\\d{2}\\)?\\s?(\\d{4,5})-?(\\d{4})$", message = "Telefone inválido! Por favor, insira um número de telefone válido.", groups = UserDTO.RequestUserPhoneInfoGroupValidation.class)
     private String phoneNumber;
 
@@ -45,13 +43,10 @@ public class UserDTO implements Serializable {
     private boolean emailVerified;
     private boolean profileVerified;
 
-    private AddressDTO address;
+    @Valid
+    private AddressFullDTO address;
 
-    public interface RequestUserNameInfoGroupValidation {
-
-    }
-
-    public interface RequestUserCPFInfoGroupValidation {
+    public interface RequestUserNameAndCPFInfoGroupValidation {
 
     }
 
