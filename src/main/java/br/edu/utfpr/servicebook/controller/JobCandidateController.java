fb3e03501920fd9a689b90f5299a6f8e6048a201
Roni Fabio Banaszewski <roni.banaszewski@gmail.com>
2022-06-27 11:42:11 -0300

    Realizados alguns ajustes na persistência de uma candidatura

--- src/main/java/br/edu/utfpr/servicebook/controller/JobCandidateController.java
+++ src/main/java/br/edu/utfpr/servicebook/controller/JobCandidateController.java
@@ -12,6 +12,7 @@ import br.edu.utfpr.servicebook.model.mapper.JobRequestMapper;
 import br.edu.utfpr.servicebook.service.IndividualService;
 import br.edu.utfpr.servicebook.service.JobCandidateService;
 import br.edu.utfpr.servicebook.service.JobRequestService;
+import br.edu.utfpr.servicebook.util.CurrentUserUtil;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,6 +20,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import javax.persistence.EntityNotFoundException;
 import java.util.Optional;
 import java.util.Set;
 
@@ -45,30 +47,23 @@ public class JobCandidateController {
 
     @PostMapping
     public ModelAndView save(JobCandidateDTO dto, RedirectAttributes redirectAttributes) {
-        //Dados fixos por enquanto, pois ainda nao existe TOKEN de autenticaçao para pegar os dados
-        Long idIndividual = 1L;
-        Optional<Long> maxCandidates = Optional.of(20L);
-        //---------------------------------------
 
-        JobRequest jobRequest = jobRequestService.findById(dto.getId()).get();
-        Individual individual = individualService.findById(idIndividual).get();
+        //simula um usuário autenticado
+        String currentUserEmail = CurrentUserUtil.getCurrentUserEmail();
 
-        JobRequestMinDTO jobRequestMinDTO = jobRequestMapper.toMinDto(jobRequest, maxCandidates);
-        dto.setJobRequest(jobRequestMinDTO);
+        Optional<Individual> oindividual = individualService.findByEmail(currentUserEmail);
 
-        IndividualMinDTO individualMinDTO = individualMapper.toMinDto(individual);
-        dto.setIndividual(individualMinDTO);
+        if(!oindividual.isPresent()){
+            throw new EntityNotFoundException("O usuário não foi encontrado!");
+        }
 
-        JobCandidate jobCandidate = jobCandidateMapper.toEntity(dto);
-        jobCandidate.setJobRequest(jobRequest);
-        jobCandidate.setIndividual(individual);
+        Optional<JobRequest> oJobRequest = jobRequestService.findById(dto.getId());
 
-        Set<JobCandidate> jobCandidates = jobRequest.getJobCandidates();
-        jobCandidates.add(jobCandidate);
+        if(!oJobRequest.isPresent()){
+            throw new EntityNotFoundException("A ordem de serviço não foi encontrada!");
+        }
 
-        jobRequest.setJobCandidates(jobCandidates);
-
-        jobRequestService.save(jobRequest);
+        JobCandidate jobCandidate = new JobCandidate(oJobRequest.get(), oindividual.get());
         jobCandidateService.save(jobCandidate);
 
         redirectAttributes.addFlashAttribute("msg", "Candidatura realizada com sucesso!");
