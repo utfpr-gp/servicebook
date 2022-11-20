be2ce1122bfc3ed4bc8231387b0fb45f4e7f4398
ricardodidimo <ricardodidimo21@gmail.com>
2022-11-20 12:02:52 -0300

    Movido o 'submit trigger' especificado na view 'professional/my-account" para dentro da lógica do script

--- src/main/webapp/assets/resources/scripts/expertise-sidepanel-ajax.js
+++ src/main/webapp/assets/resources/scripts/expertise-sidepanel-ajax.js
@@ -1,9 +1,16 @@
 $(document).ready(function() {
+    $('#select-expertise').formSelect();
+
+    $('#select-expertise').change(function () {
+        $('#form-expertise').submit();
+    });
+
     $("#form-expertise").submit(function (e) { 
         e.preventDefault();
+
         const expertiseId = e.target.elements[1].value;
-        
         const apiUrl = `/servicebook/especialidades/api/get-by-professional/${expertiseId}`
+
         $.ajax({
             type: "get",
             url: apiUrl,
@@ -14,7 +21,7 @@ $(document).ready(function() {
                 $("#expertise-comments").text(response.comments);
                 
                 $(".expertise-rating-star").each(function (index, element) {
-                    response.expertiseRating >= (index + 1) ?  element.innerText = 'star' : element.innerText = 'star_border'
+                    response.ratingScore >= (index + 1) ?  element.innerText = 'star' : element.innerText = 'star_border'
                 });
             }
         });
