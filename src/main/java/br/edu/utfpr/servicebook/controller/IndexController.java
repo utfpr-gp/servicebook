8086ee9bcc00fb19115c4a7ed97026ab5e16d1f4
Roni Fabio Banaszewski <roni.banaszewski@gmail.com>
2023-03-22 15:32:59 -0300

    refactor: variáveis de ambiente no arquivo .env

--- src/main/java/br/edu/utfpr/servicebook/controller/IndexController.java
+++ src/main/java/br/edu/utfpr/servicebook/controller/IndexController.java
@@ -8,6 +8,7 @@ import br.edu.utfpr.servicebook.service.IndividualService;
 import br.edu.utfpr.servicebook.util.sidePanel.SidePanelIndividualDTO;
 import br.edu.utfpr.servicebook.util.sidePanel.SidePanelUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.security.authentication.AnonymousAuthenticationToken;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
