2f8ec6d08b08218fe7a593405b4b4a916aa65590
TaisHryssai <thryssai@gmail.com>
2023-06-13 22:51:03 -0300

    Cadastro de categorias

---
+++ src/main/java/br/edu/utfpr/servicebook/model/dto/CategoryDTO.java
@@ -0,0 +1,20 @@
+package br.edu.utfpr.servicebook.model.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+import javax.validation.constraints.NotBlank;
+import java.io.Serializable;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@ToString
+public class CategoryDTO  implements Serializable {
+    private Long id;
+
+    @NotBlank(message = "O nome da categoria é obrigatório")
+    private String name;
+}
